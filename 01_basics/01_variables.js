6ec3e0ea68bccacf11d62b5585f092632d38e3b3
jalajdave26 <162538975+jalajdave26@users.noreply.github.com>
2024-07-29 09:58:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1445533
+let accountEmail = "jdkingf123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmedabad"
+
+// accountId = 3 // not allowed
+console.log(accountId);
+
+accountEmail = "jdjd@gmail.com"
+accountPassword = "121212"
+accountCity = "Jaipur"
+
+console.table([ accountId, accountEmail, accountPassword, accountCity])
